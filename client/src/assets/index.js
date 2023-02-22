953fd9f86e9eae36ef26b77ebb0cc295f811d9e2
tong2984a <tong2984a@gmail.com>
2023-02-22 23:00:23 +0800

    init

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import funeral from './funeral.png';
+
+export {
+  download,
+  logo,
+  funeral,
+  preview,
+};
